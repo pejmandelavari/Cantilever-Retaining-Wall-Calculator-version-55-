d39e88177f74ef08229778c48834dbb991c854ef
pejmandelavari <pezhman.delavari@gmail.com>
2026-01-08 22:41:00 -0500

    Update ret_wall_v55.py

--- ret_wall_v55.py
+++ ret_wall_v55.py
@@ -11,7 +11,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Cantilever Retaining Wall Calculator",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 st.markdown("""
